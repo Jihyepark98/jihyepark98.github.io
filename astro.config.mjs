b9f491d4d461985c71215c372b4ba600fccd8d17
Jihyepark98 <86645899+Jihyepark98@users.noreply.github.com>
2026-02-07 15:13:55 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jihyepark98.github.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
